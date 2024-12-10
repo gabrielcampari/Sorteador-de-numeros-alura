cff1a2d20d0b40b617ebec27e43c7439997752a3
gabrielcampari <camparicorreia@gmail.com>
2024-12-10 14:31:43 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+  //Objetivo de receber os valores: "Quantidade de números, do número e Até o número", use os id para receber esses valores
+
+  let quantidade = document.getElementById("quantidade").value;
+  let de = document.getElementById("de").value;
+  let ate = document.getElementById("ate").value;
+
+  alert(quantidade);
+}
