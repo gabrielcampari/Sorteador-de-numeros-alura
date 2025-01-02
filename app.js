87e191c4ca1bc50cafc7e18de2e104c96df2f810
Gabriel Correia <camparicorreia@gmail.com>
2025-01-01 22:24:13 -0300

    feat/Add sort number function

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-function sortear() {
+function raffle() {
   //Objetivo de receber os valores: "Quantidade de números, do número e Até o número", use os id para receber esses valores
 
-  let quantidade = document.getElementById("quantidade").value;
-  let de = document.getElementById("de").value;
-  let ate = document.getElementById("ate").value;
+  let quantidade = parseInt(document.getElementById("quantidade").value);
+  let de = parseInt(document.getElementById("de").value);
+  let ate = parseInt(document.getElementById("ate").value);
 
-  alert(quantidade);
+  let number = takeRandomNumber(de, ate);
+  alert(`O numero sorteado foi: ${number}`);
+}
+
+function takeRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min - 1)) + min;
 }
